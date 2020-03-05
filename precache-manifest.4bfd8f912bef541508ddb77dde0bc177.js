e845d07f5232267aaaa6c5214d6ed5cfd5f4ec31
Soniella <soniellayumang@gmail.com>
2020-03-05 14:47:42 +0800

    Updates

---
+++ precache-manifest.4bfd8f912bef541508ddb77dde0bc177.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0b5ba4d3cab28746e8fe51f0a0ab5a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3783e269a42567a64821",
+    "url": "/monsters-rolodex/static/css/main.693f6039.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3783e269a42567a64821",
+    "url": "/monsters-rolodex/static/js/main.454f1295.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
